bee6d3633f1786624989b8710f1423f04d08deec
jordan smith <jordan.smith6035@gmail.com>
2023-03-02 08:36:05 -0700

    Add website link to cards

--- src/component/BreweryDetails/BreweryDetails.js
+++ src/component/BreweryDetails/BreweryDetails.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Map, {Marker} from 'react-map-gl';
+import Map, { Marker } from "react-map-gl";
 import "./BreweryDetails.css";
 import { getSingleBrewery } from "../../apicall";
-import 'mapbox-gl/dist/mapbox-gl.css'
+import "mapbox-gl/dist/mapbox-gl.css";
 import { Link } from "react-router-dom";
 
 class BreweryDetails extends React.Component {
@@ -42,31 +42,55 @@ class BreweryDetails extends React.Component {
   }
 
   render() {
-    const MAPBOX_TOKEN = 'pk.eyJ1IjoiamF5c21pdGg2MDM1IiwiYSI6ImNsZXEzNHh3cjBpN2YzcG52NGExNWgxZ2EifQ.hl74HXe1p7RQMumuu7RaSg'
+    const MAPBOX_TOKEN =
+      "pk.eyJ1IjoiamF5c21pdGg2MDM1IiwiYSI6ImNsZXEzNHh3cjBpN2YzcG52NGExNWgxZ2EifQ.hl74HXe1p7RQMumuu7RaSg";
     return (
       <div className="single-brewery-details">
         <div className="back-button">
-            <Link to="/">
+          <Link to="/">
             <button>Back to Home</button>
-            </Link>
+          </Link>
         </div>
         <h2 className="single-brewery-name">{this.state.singleBrewery.name}</h2>
-        <p className="single-brewery-address">{this.state.singleBrewery.street} {this.state.singleBrewery.city}, {this.state.singleBrewery.state}</p>
-        <p className="single-brewery-phone">Phone Number: {this.state.singleBrewery.phone}</p>
-        {this.state.viewport.latitude === 0 ? <h3>Loading...</h3>:
-        <Map
-          initialViewState={{
-            latitude: this.state.viewport.latitude,
-            longitude: this.state.viewport.longitude,
-            zoom: 18,
-          }}
-          style={{ width: this.state.viewport.width, height: this.state.viewport.height }}
-          mapStyle="mapbox://styles/mapbox/streets-v9"
-          mapboxAccessToken={MAPBOX_TOKEN}
-        >
-          <Marker longitude={this.state.viewport.longitude} latitude={this.state.viewport.latitude} color="red" />
-        </Map>
-        }
+        <p className="single-brewery-address">
+          {this.state.singleBrewery.street} {this.state.singleBrewery.city},{" "}
+          {this.state.singleBrewery.state}
+        </p>
+        <p className="single-brewery-phone">
+          Phone Number: {this.state.singleBrewery.phone}
+        </p>
+        <p className="single-brewery-website">
+          <a
+            href={this.state.singleBrewery.website_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Visit The Website
+          </a>
+        </p>
+        {this.state.viewport.latitude === 0 ? (
+          <h3>Loading...</h3>
+        ) : (
+          <Map
+            initialViewState={{
+              latitude: this.state.viewport.latitude,
+              longitude: this.state.viewport.longitude,
+              zoom: 18,
+            }}
+            style={{
+              width: this.state.viewport.width,
+              height: this.state.viewport.height,
+            }}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            mapboxAccessToken={MAPBOX_TOKEN}
+          >
+            <Marker
+              longitude={this.state.viewport.longitude}
+              latitude={this.state.viewport.latitude}
+              color="red"
+            />
+          </Map>
+        )}
       </div>
     );
   }
